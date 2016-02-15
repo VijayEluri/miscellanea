9e160aa884b60d5a799bc5a7f5c63c21f18fffa0
Ratandeep S. Ratti <rdsr.me@gmail.com>
2016-02-15 13:57:41 -0800

    Update clojure to 1.8 and minor refactoring

--- encrypt/project.clj
+++ encrypt/project.clj
@@ -1,4 +1,3 @@
 (defproject encrypt "1.0.0.0"
   :description "encrypting/decrypting files"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
--- encrypt/src/encrypt/core.clj
+++ encrypt/src/encrypt/core.clj
@@ -1,47 +1,48 @@
 (ns encrypt.core
   (:require [clojure.java.io :as io])
   (:import [java.io File ByteArrayOutputStream]
-           [javax.crypto KeyGenerator Cipher SecretKeyFactory]
-           [javax.crypto.spec PBEParameterSpec PBEKeySpec]))
+           [javax.crypto Cipher SecretKeyFactory]
+           [javax.crypto.spec PBEParameterSpec PBEKeySpec]
+           (java.lang.reflect Type)))
 
 (def salt (byte-array (map byte [0x1 0x10 0x30 0x40 0x34 0x24 0x11 0x23])))
 (def cnt 20)
 (def algorithm "PBEWithMD5AndDES")
 
-(defn password []
-  (println "Enter password for encryption/decryption")
-  (.readPassword (System/console)))
+(def ^:dynamic ^String password)
 
 (defn gen-key []
-  (let [keyspec (PBEKeySpec. (password))]
+  (let [keyspec (PBEKeySpec. (char-array password))]
     (-> algorithm
-         SecretKeyFactory/getInstance
-         (.generateSecret keyspec))))
+        SecretKeyFactory/getInstance
+        (.generateSecret keyspec))))
 
-(defn process [data mode]
+(defn run-cipher [data mode]
   (let [pbe-key (gen-key)
         spec (PBEParameterSpec. salt cnt)
         cipher (Cipher/getInstance algorithm)]
     (.init cipher mode pbe-key spec)
     (.doFinal cipher data)))
 
-(defn encrypt [data]
-  (process data Cipher/ENCRYPT_MODE))
+(defn encrypt-bytes [^bytes data]
+  (run-cipher data Cipher/ENCRYPT_MODE))
 
-(defn decrypt [data]
-  (process data Cipher/DECRYPT_MODE))
+(defn decrypt-bytes [^bytes data]
+  (run-cipher data Cipher/DECRYPT_MODE))
 
-(defn as-bytes [file]
+(defn as-bytes [^File file]
   (let [os (ByteArrayOutputStream.)]
     (io/copy file os)
     (.toByteArray os)))
 
-(defn main [args]
-  (let [mode (first args)
-        source (-> args second File.)
-        target (-> args last File.)]
-    (if (= mode "encrypt")
-      (-> source as-bytes encrypt (io/copy target))
-      (-> source as-bytes decrypt (io/copy target)))))
+(defn encrypt
+  [^String src-path ^String dst-path]
+  (let [src (File. src-path)
+        dst (File. dst-path)]
+    (-> src as-bytes encrypt-bytes (io/copy dst))))
 
-(main *command-line-args*)
+(defn decrypt
+  [^String src-path ^String dst-path]
+  (let [src (File. src-path)
+        dst (File. dst-path)]
+    (-> src as-bytes decrypt-bytes (io/copy dst))))
